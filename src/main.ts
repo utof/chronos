fde21a3e6c3f6ea1a5557a9c91a54b5b6e323008
utof <bondar030@mail.ru>
2024-09-09 22:29:01 +0900

    chronoview

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,18 @@
-import { Notice, Plugin } from "obsidian";
+import { Notice, Plugin, WorkspaceLeaf } from "obsidian";
 import { ChronosModal } from "./modal";
+import { ChronosView, VIEW_TYPE_CHRONOS } from "./chronoview";
 
 export default class Chronos extends Plugin {
 	async onload() {
+		this.registerView(VIEW_TYPE_CHRONOS, (leaf) => new ChronosView(leaf));
+
 		new Notice("Chronos loaded!");
+
 		this.addRibbonIcon("clock", "Chronos", () => {
 			// Creating a function that will display all the notes in chronological order as a modal
 
 			new Notice("This is a notice!");
-
+			this.activateView();
 			// const modal = new ChronosModal(this.app).open();
 		});
 	}
@@ -17,5 +21,29 @@ export default class Chronos extends Plugin {
 	// How do i know when to write New and when not? A: This will come with experience, but generally, if you are working with a class, you will need to use the new keyword.
 	async onunload() {
 		new Notice("Chronos unloaded");
+	}
+
+	async activateView() {
+		const { workspace } = this.app;
+
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(VIEW_TYPE_CHRONOS);
+
+		if (leaves.length > 0) {
+			// A leaf with our view already exists, use that
+			leaf = leaves[0];
+		} else {
+			// Our view could not be found in the workspace, create a new leaf
+			// in the right sidebar for it
+			leaf = workspace.getRightLeaf(true);
+			// leaf = workspace.getLeaf(true);
+			await leaf.setViewState({ type: VIEW_TYPE_CHRONOS, active: true });
+		}
+		// "Reveal" the leaf in case it is in a collapsed sidebar
+		// make new window beforehand:
+
+		workspace.revealLeaf(leaf);
+
+		// workspace.revealLeaf(leaf);
 	}
 }
